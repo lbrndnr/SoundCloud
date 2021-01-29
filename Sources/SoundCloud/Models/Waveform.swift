e4c2d4b9c26add781e164dfbd046419f1b8beb8b
Laurin Brandner <mail@laurinbrandner.ch>
2021-01-29 22:24:23 +0100

    Add waveform init

--- Sources/SoundCloud/Models/Waveform.swift
+++ Sources/SoundCloud/Models/Waveform.swift
@@ -7,11 +7,26 @@
 
 import Foundation
 
-public struct Waveform: Decodable {
+public struct Waveform {
     
     public var width: Int
     public var height: Int
     public var samples: [Int]
+    
+    public init(width: Int, height: Int, samples: [Int]) {
+        self.width = width
+        self.height = height
+        self.samples = samples
+    }
+    
+    public init(width: Int, height: Int, repeatedSample: Int) {
+        let samples = Array(repeating: repeatedSample, count: width)
+        self.init(width: width, height: height, samples: samples)
+    }
+    
+}
+
+extension Waveform: Decodable {
     
     enum CodingKeys: String, CodingKey {
         case width
